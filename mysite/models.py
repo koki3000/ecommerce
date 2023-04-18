74467a6b2f0eada98a79bd375a66f889885bf62d
Konrad P <kokiziom@gmail.com>
2023-04-18 17:15:42 +0200

    Added Product model

--- mysite/models.py
+++ mysite/models.py
@@ -8,3 +8,12 @@ class Category(models.Model):
 
     def __str__(self):
         return f'{self.name}'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50, null=True, blank=True)
+    price =  models.FloatField()
+    category = models.ForeignKey(Category, models.PROTECT, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    availability = models.BooleanField()
+    quantity = models.IntegerField()
