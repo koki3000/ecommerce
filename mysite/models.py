df07abe0ddcb986d27e77e55a91f7b9bce8d7af5
Konrad P <kokiziom@gmail.com>
2023-04-18 17:09:03 +0200

    Added Category model

--- mysite/models.py
+++ mysite/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return f'{self.name}'
