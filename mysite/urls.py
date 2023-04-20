6a39e054d93401e2861dccc09eaf7cd3a39774ac
Konrad P <kokiziom@gmail.com>
2023-04-20 12:49:40 +0200

    Added Product detail view

--- mysite/urls.py
+++ mysite/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('product/create/', views.CreateProductView.as_view(), name="create-product"),
     path('product/<int:pk>/delete/', views.DeleteProductView.as_view(), name="delete-product"),
     path('product/<int:pk>/update/', views.UpdateProductView.as_view(), name="update-product"),
+    path('product/<int:pk>/', views.ProductView.as_view(), name="details-product"),
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -1,4 +1,5 @@
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.urls import path, reverse_lazy, reverse
 from . models import Product, Category
@@ -16,6 +17,12 @@ class HomePageView(ListView):
         context = super().get_context_data(**kwargs)
         context["category_list"] = Category.objects.all()
         return context
+    
+
+class ProductView(DetailView):
+    
+    model = Product
+    template_name = 'mysite/detail_product.html'
 
 class CreateProductView(CreateView):
 
@@ -36,5 +43,8 @@ class UpdateProductView(UpdateView):
 
     model = Product
     form_class = ProductForm
-    success_url = reverse_lazy("home")
     template_name = 'mysite/update_product_form.html'
+
+
+    def get_success_url(self):
+        return reverse('details-product', kwargs={'pk': self.object.id})
