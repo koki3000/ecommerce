10ba5954ab21cdbd17b109e7194fa6491e199d3e
Konrad P <kokiziom@gmail.com>
2023-04-26 14:13:14 +0200

    Changed fields names in forms

--- mysite/models.py
+++ mysite/models.py
@@ -6,19 +6,19 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Category(models.Model):
 
-    name = models.CharField(max_length=50, unique=True)
+    name = models.CharField('Nazwa', max_length=50, unique=True)
 
     def __str__(self):
         return f'{self.name}'
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=50, null=True, blank=True)
-    price =  models.FloatField(validators=[MinValueValidator(0)])
-    category = models.ForeignKey(Category, models.PROTECT, null=True, blank=True)
-    description = models.TextField(null=True, blank=True)
-    availability = models.BooleanField()
-    quantity = models.PositiveIntegerField()
+    name = models.CharField('Nazwa', max_length=50, null=True, blank=True)
+    price =  models.FloatField('Cena', validators=[MinValueValidator(0)])
+    category = models.ForeignKey(Category, models.PROTECT, 'Kategoria', null=True, blank=True)
+    description = models.TextField('Opis', null=True, blank=True)
+    availability = models.BooleanField('Dostępność')
+    quantity = models.PositiveIntegerField('Ilość w magazynie')
 
     def __str__(self):
         return f'{self.name}'
@@ -26,8 +26,8 @@ class Product(models.Model):
 
 class Basket(models.Model):
     owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
-    product = models.ForeignKey(Product, models.PROTECT, null=True, blank=True)
-    quantity = models.PositiveIntegerField(validators=[MinValueValidator(1)], null=True, blank=True, default=1)
+    product = models.ForeignKey(Product, models.PROTECT, 'Produkt', null=True, blank=True)
+    quantity = models.PositiveIntegerField('Ilość', validators=[MinValueValidator(1)], null=True, blank=True, default=1)
     
     def __str__(self):
         return f'{self.product}'
