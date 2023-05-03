41d4d42f5692d2eecfe7df95754949ee37d35385
Konrad P <kokiziom@gmail.com>
2023-05-03 20:18:29 +0200

    Added Delivery views

--- mysite/views.py
+++ mysite/views.py
@@ -2,8 +2,8 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.urls import path, reverse_lazy, reverse
-from . models import Product, Category, OrderDetail
-from . forms import ProductForm, CategoryForm, UserForm, OrderDetailForm, ProductSearchForm
+from . models import Product, Category, OrderDetail, Delivery
+from . forms import ProductForm, CategoryForm, UserForm, OrderDetailForm, ProductSearchForm, DeliveryForm
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
@@ -259,3 +259,43 @@ def buy_view(request):
         messages.success(request, f'Produkty zamówione.')
         home = request.POST.get('home', '/')
         return HttpResponseRedirect(home)
+    
+
+# Delivery Views
+
+class DeliveryPageView(ListView):
+
+    model = Delivery
+    template_name = 'mysite/delivery/delivery_list.html'
+    
+
+class DeliveryView(DetailView):
+    
+    model = Delivery
+    template_name = 'mysite/delivery/detail_delivery.html'
+
+
+class CreateDeliveryView(CreateView):
+
+    model = Delivery
+    form_class = DeliveryForm
+    success_url = reverse_lazy("delivery")
+    template_name = 'mysite/delivery/create_delivery_form.html'
+
+
+class DeleteDeliveryView(DeleteView):
+
+    model = Delivery
+    success_url = reverse_lazy("delivery")
+    template_name = 'mysite/delivery/delete_delivery_form.html'
+
+
+class UpdateDeliveryView(UpdateView):
+
+    model = Delivery
+    form_class = DeliveryForm
+    template_name = 'mysite/delivery/update_delivery_form.html'
+
+
+    def get_success_url(self):
+        return reverse('details-delivery', kwargs={'pk': self.object.id})
