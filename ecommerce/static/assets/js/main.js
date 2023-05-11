3b376afde2fd5f1d79e868369724581aa9cfe17b
Konrad P <kokiziom@gmail.com>
2023-05-11 10:32:58 +0200

    Added slider to product_list.html

---
+++ ecommerce/static/assets/js/main.js
@@ -0,0 +1,29 @@
+var slide_number = 0;
+var timer1 = 0;
+var timer2 = 0;
+var slides_tab = ['Aeropress', 'Comandante', 'Ekspres kolbowy', 'Waga', 'Zielona']
+
+const hide = () =>
+{
+    $("#slider").fadeOut(500);
+}
+
+const slides = () =>
+{
+    var file = "<img src=\"/static/assets/img/"+ slides_tab[slide_number] +".jpg\">";
+    slide_number++;
+    if (slide_number>4) slide_number=0;
+    document.getElementById("slider").innerHTML = file;
+    $("#slider").fadeIn(500);
+    timer1 = setTimeout("slides()", 5000);
+    timer2 = setTimeout("hide()", 4500);
+}
+
+const set_slide = (nr) => 
+{
+    clearTimeout(timer1);
+    clearTimeout(timer2);
+    slide_number = nr;
+    hide();
+    setTimeout("slides()", 500);
+}
