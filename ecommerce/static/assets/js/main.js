45b4479073d1388144cb72bb66ed99d2055ba9a5
Konrad P <kokiziom@gmail.com>
2023-05-12 15:46:15 +0200

    Added dynamic creation of slider nav

--- ecommerce/static/assets/js/main.js
+++ ecommerce/static/assets/js/main.js
@@ -2,6 +2,7 @@ var slide_number = 0;
 var timer1 = 0;
 var timer2 = 0;
 var slides_tab = ['Aeropress', 'Comandante', 'Ekspres kolbowy', 'Waga', 'Zielona']
+var slide_nav = '';
 
 const hide = () =>
 {
@@ -11,6 +12,8 @@ const hide = () =>
 const slides = () =>
 {
     var file = "<a href=\"/slider/"+ slides_tab[slide_number] +"/\"><img src=\"/static/assets/img/"+ slides_tab[slide_number] +".jpg\"></a>";
+    
+
     slide_number++;
     if (slide_number>4) slide_number=0;
     document.getElementById("slider").innerHTML = file;
@@ -27,5 +30,19 @@ const set_slide = (nr) =>
     hide();
     setTimeout("slides()", 500);
 }
+    
+function setup()
+{
+    for (let index = 0; index < slides_tab.length; index++) {
+        slide_nav = slide_nav + "<span id=\"slide"+ index +"\" style=\"cursor: pointer;\">["+ (index + 1) +"]</span>";
+    }
+    $('#slide_nav').html(slide_nav);
+    for (let index = 0; index < slides_tab.length; index++) {
+        document.getElementById('slide'+index).addEventListener('click', function(){
+            set_slide(index);
+        });
+    }
+    slides();
+}
 
-window.onload = slides;
+window.onload = setup;
